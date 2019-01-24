b4e9ecc6851024266263a29ed5cb6d1b899afa8e
Chemist <374224974@qq.com>
2019-01-24 11:46:46 +0800

    fasttext
    fasttext classificial

--- Inter_reamrk_fasttext.py
+++ Inter_reamrk_fasttext.py
@@ -1,77 +1,23 @@
 #！/root/anaconda3/bin/python3.6
 # coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import random
 import jieba 
 import jieba.analyse
-from gensim.models import word2vec 
-from gensim.models.doc2vec import Doc2Vec, TaggedDocument
-from sklearn.preprocessing import scale
-from xgboost.sklearn import XGBClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import StratifiedKFold
-import matplotlib.pyplot as plt
 import warnings
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.layers.embeddings import Embedding
-from keras.models import Sequential
-from keras.layers import *
-from keras.optimizers import SGD
-from keras.utils import to_categorical
-from keras import layers
-from keras.callbacks import EarlyStopping,ReduceLROnPlateau,ModelCheckpoint
-from keras.models import Model
-from keras.models import Model,load_model
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score,accuracy_score,recall_score,precision_score
-from keras import backend as K
-from sklearn.model_selection import StratifiedKFold
-from snownlp import SnowNLP
-from keras.utils import multi_gpu_model
 import fasttext
-import gc
-import re
-import os
+
 #os.environ["CUDA_VISIBLE_DEVICES"] = '1'
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from sklearn.metrics import f1_score,accuracy_score
 warnings.filterwarnings(action='ignore', category=DeprecationWarning)
-warnings.filterwarnings(action='ignore', category=UserWarning,module = 'gensim' )
-from keras.callbacks import Callback
-from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
-class Metrics(Callback):
-
-    def on_train_begin(self, logs={}):
-        self.val_f1s = []
-        self.val_recalls = []
-        self.val_precisions = []
-        self.best_f1 = 0
-
-    def on_epoch_end(self, epoch, logs={}):
-        val_predict = np.argmax(np.asarray(self.model.predict(self.validation_data[0])), axis=1)
-        val_targ = np.argmax(self.validation_data[1], axis=1)
-        _val_f1 = f1_score(val_targ, val_predict, average='macro')
-        #_val_recall = recall_score(val_targ, val_predict)
-        #_val_precision = precision_score(val_targ, val_predict)
-        #print('验证集recall为{}'.format(_val_recall))
-        #print('验证集precision为{}'.format(_val_precision))
-        print('验证集macro f1 {}'.format(_val_f1))
-        if _val_f1 > self.best_f1:
-            self.best_f1 = _val_f1
-            self.model.save('bi_gru.h5')
-        return
 
 print('读取数据')
 
 train = pd.read_csv('train.csv')
-valid = pd.read_csv('data_sample.csv')
-#test_a = pd.read_csv('sentiment_analysis_testa.csv')
+valid = pd.read_csv('valid.csv')
+#test_a = pd.read_csv('test.csv')
 
 words = [train,valid]
 #生成训练文件
@@ -142,7 +88,5 @@ F1_score = f1_score(y_val_pred, val_y, average='macro')
 print('模型的macro f1为：{}'.format(F1_score))
 #y_test_pred = np.argmax(y_test_pred, axis=1)
 
-
-#out.to_csv('bi_gru_shuju1.csv',index = False)
 print('***************************finish****************************')
 
